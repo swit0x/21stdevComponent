77db5e1581341aa88377e9cb8ee5a7f42f79c4a0
swit0x <therawswitch@gmail.com>
2026-06-15 01:23:54 +0300

    Update specific file content

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
+'use client'
+import React from "react";
+import { WavePath } from "@/components/wave-path";
+
 import { DottedSurface } from "@/components/dotted-surface";
 import { LogoCloud } from "@/components/logo-cloud-2";
 import { FallingPattern } from "@/components/falling-pattern";
@@ -51,6 +55,30 @@ export default function DemoOne() {
         <div className="absolute inset-0 z-10 flex items-center justify-center">
           <h1 className="font-sans text-4xl font-extrabold tracking-tighter">
             or you can be in a matrix simulation using NextJS, ShadCN and 21st.dev          </h1>
+        </div>
+      </div>
+
+      <div className="relative w-full flex min-h-screen flex-col items-center justify-center">
+        <div
+          aria-hidden="true"
+          className={cn(
+            'pointer-events-none absolute -top-10 left-1/2 size-full -translate-x-1/2 rounded-full',
+            'bg-[radial-gradient(ellipse_at_center,--theme(--color-foreground/.1),transparent_50%)]',
+            'blur-[30px]',
+          )}
+        />
+
+        <div className="flex w-[70vw] flex-col items-end">
+          <WavePath className="mb-10" />
+          <div className="flex w-full flex-col items-end">
+            <div className="flex justify-end">
+              <p className="text-muted-foreground mt-2 text-sm">World of Art</p>
+              <p className="text-foreground/80 ml-8 w-3/4 text-2xl md:text-4xl">
+                Experience the emotions of artists through their works. Let the
+                beauty of art inspire you and fill your soul.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </>
